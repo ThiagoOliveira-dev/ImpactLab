4b839475ed5542f29bcf4af8ccbc990f71c3d2a5
Thiago <tw180896@gmail.com>
2023-05-03 20:54:21 -0400

    main

--- basico/main.cpp
+++ basico/main.cpp
@@ -31,48 +31,14 @@ int main(int argc, char const *argv[])
     write << width << " " << heigt<< endl;
     write << RGB << endl;
 
-    while (!read.eof())
+   while (!read.eof())
         {
             read >> red;
-            intR = stoi(red);
-            write << intR << endl;
-
             read >> gree;
-            intG = stoi(gree);
-            write << intG << endl;
-
             read >> blue;
-            intB = stoi(blue);
-            write << intB << endl;
 
-            write << endl;
+            write << red << " " << gree << " " << blue <<endl;
         }
-
-    /*if (read.is_open()){
-        while (getline(read, line))
-        {
-            if(!read.eof()){
-             write << line << endl;
-            }
-        }
-        
-    }*/
-
     
-   /*
-    file << "p3"<<endl;
-    file << "250 250"<<endl;
-    file << "255"<<endl;
-
-    for (int i = 0; i < 250; i++)
-    {
-        for (int j = 0; j < 250; j++)
-        {
-            file << j << " " << j << " " << j << endl;
-        }
-    }*/
-    
-    write.close();
-    read.close();
     return 0;
 }
